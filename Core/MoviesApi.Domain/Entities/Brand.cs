2e6455567c0d9aa0147ee87668ab626733308c58
Eyyub Saral <eyyubsrl04@gmail.com>
2025-08-06 15:48:38 +0300

    Configuration lar oluşturuldu

--- Core/MoviesApi.Domain/Entities/Brand.cs
+++ Core/MoviesApi.Domain/Entities/Brand.cs
@@ -18,6 +18,6 @@ namespace MoviesApi.Domain.Entities
         {
             Name = name;
         }
-        public required string Name { get; set; };
+        public required string Name { get; set; }
     }
 }
---
+++ İnfrastructure/MoviesApi.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoviesApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            Category category1 = new()
+            {
+                Id = 1,
+                Name = "Elektirik",
+                Priorty=1,
+                ParentID=0,
+                isDeleted=false,
+                CreatedDate=DateTime.Now,
+            };
+
+            Category category2 = new()
+            {
+                Id = 2,
+                Name = "Moda",
+                Priorty = 2,
+                ParentID = 0,
+                isDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            Category parent1 = new()
+            {
+                Id = 3,
+                Name = "Bilgisayar",
+                Priorty = 1,
+                ParentID = 1,
+                isDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            Category parent2 = new()
+            {
+                Id = 4,
+                Name = "Kadın",
+                Priorty = 1,
+                ParentID = 2,
+                isDeleted = false,
+                CreatedDate = DateTime.Now,
+            };
+
+            builder.HasData(category1,category2,parent1,parent2);
+        }
+    }
+}

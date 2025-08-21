1bc484923e9c4a567e4ff3a54b1886aef18885d3
Eyyub Saral <eyyubsrl04@gmail.com>
2025-08-21 12:36:39 +0300

    register işlemleri tamamlandı

---
+++ Core/MoviesApi.Application/Features/Auth/Rules/AuthRules.cs
@@ -0,0 +1,20 @@
+﻿using MoviesApi.Application.Bases;
+using MoviesApi.Application.Features.Auth.Exceptions;
+using MoviesApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Application.Features.Auth.Rules
+{
+    public class AuthRules:BaseRules
+    {
+        public Task UserShouldNotBeExist(User? user)
+        {
+            if (user is not null) throw new UserAlreadyExistException();
+            return Task.CompletedTask;
+        }
+    }
+}
